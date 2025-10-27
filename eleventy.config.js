ab07d05d974bf235d6989341662a3a412c399da8
Brad Jeschonek <blj747@gmail.com>
2025-10-26 23:55:00 -0400

    updated base and footer, added helpers function

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,8 +1,16 @@
 module.exports = (config) => {
+    // Passthrough Directories
+    config.addPassthroughCopy('./src/images/');
+
+    // Tell 11ty to use the .eleventyignore instead of .gitignore
+    config.setUseGitIgnore(false);
+
     return {
+        // Allows use of .html files instead of .njk
         markdownTemplateEngine: 'njk',
         dataTemplateEngine: 'njk',
         htmlTemplateEngine: 'njk',
+        // Input and output folder names
         dir: {
             input: 'src',
             output: 'dist',
