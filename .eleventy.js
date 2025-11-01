a8cc96df06d69b81894b5d4258270a421c4c01d8
Brad Jeschonek <blj747@gmail.com>
2025-10-31 23:27:59 -0400

    Modified config, eleventyignore, site.json, base, home

--- .eleventy.js
+++ .eleventy.js
@@ -1,12 +1,14 @@
+// 11ty Config File
 module.exports = (eleventyConfig) => {
 
     // Tell 11ty to use the .eleventyignore instead of .gitignore
     config.setUseGitIgnore(false);
 
     return {
-        // Allows use of Nunjucks within .html files instead of .njk files
         markdownTemplateEngine: 'njk',
         dataTemplateEngine: 'njk',
+        
+        // Allows use of Nunjucks within .html files instead of .njk files
         htmlTemplateEngine: 'njk',
 
         // Changing default input and output folder names
