1d445c104a7b3dbb70a859ff0bbecd229c6d665d
Brad Jeschonek <blj747@gmail.com>
2025-11-05 00:05:50 -0500

    added luxon, modified post, added date filters to config

--- .eleventy.js
+++ .eleventy.js
@@ -1,11 +1,24 @@
+const { DateTime } = require('luxon');
 const rssPlugin = require('@11ty/eleventy-plugin-rss');
+const readingTime = require('eleventy-plugin-reading-time');
+
 
 module.exports = (eleventyConfig) => {
     // Plugins
     eleventyConfig.addPlugin(rssPlugin);
+    eleventyConfig.addPlugin(readingTime);
     
     // Tell 11ty to use .eleventyignore instead of .gitignore
-    config.setUseGitIgnore(false);
+    eleventyConfig.setUseGitIgnore(false);
+
+    // Custom Filters
+    eleventyConfig.addFilter('createReadableDate', (dateObj) => {
+        return DateTime.fromJSDate(dateObj, { zone: 'utc' }).toFormat('LLL dd yyyy');
+    });
+
+    eleventyConfig.addFilter('htmlDateString', (dateObj) => {
+        return DateTime.fromJSDate(dateObj, { zone: 'utc' }).toFormat('yyyy-LL-dd');
+    });
 
     return {
         // Defines template engines, allows for use of .html files instead of .njk
