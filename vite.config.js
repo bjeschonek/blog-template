44be326794e3f2f0916f12688a05d697a73fc4e8
Brad Jeschonek <blj747@gmail.com>
2025-11-18 16:47:51 -0500

    added tailwind as a postcss plugin to see if serving works and it's not

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,4 @@
 import { defineConfig } from 'vite';
-import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
     publicDir: 'public',
@@ -27,7 +26,4 @@ export default defineConfig({
             }
         }
     },
-    plugins: [
-        tailwindcss()
-    ]
 });
