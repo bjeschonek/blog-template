e58fd1d268471491074506c870da8551b70e7499
Brad Jeschonek <blj747@gmail.com>
2025-11-26 18:14:02 -0500

    fixed newsletter js error

--- src/assets/js/newsletter.js
+++ src/assets/js/newsletter.js
@@ -23,7 +23,7 @@ export default function newsletterSubscribe() {
                 headers: {
                     'Content-Type': 'application/json'
                 },
-                body: JSON.stringify({ email });
+                body: JSON.stringify({ email })
             });
 
             if (response.ok) {
