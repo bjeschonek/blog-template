4d5a62aad98e1450fa583d0f2b309c5e8025aa44
Brad Jeschonek <blj747@gmail.com>
2025-12-01 20:45:44 -0500

    working on contact form submission script

--- src/assets/js/contact.js
+++ src/assets/js/contact.js
@@ -1,8 +1,72 @@
-export default function handleContactFormSubmission () {
+export default function handleContactFormSubmission() {
     const contactForm = document.querySelector('#contact-form');
+    const contactHeading = document.querySelector('#contact-heading');
+    const firstNameInput = document.querySelector('#first-name');
+    const lastNameInput = document.querySelector('#last-name');
+    const emailInput = document.querySelector('#email');
+    const phoneNumberInput = document.querySelector('#phone-number');
+    const messageTextInput = document.querySelector('#message');
+    const contactSubmitBtn = document.querySelector('#contact-submit');
+    const contactSuccessMsg = document.querySelector('#contact-success');
 
     contactForm.addEventListener('submit', async (event) => {
         event.preventDefault();
+
+        const firstName = firstNameInput.value.trim();
+        const lastName = lastNameInput.value.trim();
+        const email = emailInput.value;
+        const phoneNumber = phoneNumberInput.value;
+        const messageText = messageTextInput.value;
+
+        if (!isValidName(firstName)) {
+            showInfo('Please enter a valid first name.', 'error');
+            return;
+        }
+
+        if (!isValidName(lastName)) {
+            showInfo('Please enter a valid last name', 'error');
+            return;
+        }
+
+        if (!isValidEmail(email)) {
+            showInfo('Please enter a valid email address', 'error');
+            return;
+        }
+
+        if (!isValidPhone(phoneNumber)) {
+            showInfo('Please enter a valid phone number.', 'error');
+            return;
+        }
+
+        if (!isValidMessage(messageText)) {
+            showInfo('Please enter a message', 'error');
+            return;
+        }
+
+        contactSubmitBtn.disabled = true;
+        showInfo('Attempting to send message...', 'info');
+
+        try {
+            const response = await fetch('http://localhost:5173', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ firstName, lastName, email, phoneNumber, message })
+            });
+            if (response.ok) {
+                contactForm.style.display = 'hidden';
+                contactHeading.style.display = 'hidden';
+                contactSuccessMsg.style.display = 'block';
+            } else {
+                const errorData = await response.json();
+                showInfo('Error sending message.', 'error');
+            }
+        } catch (error) {
+            showInfo('An unknown error occurred, please try again later.', 'error');
+        } finally {
+            contactSubmitBtn.disabled = false;
+        }
     });
 
     function isValidName(name) {
@@ -23,7 +87,7 @@ export default function handleContactFormSubmission () {
     }
 
     function isValidPhone(number) {
-        const phoneRegex = /^[\+]?[0-9]{0,3}\W?+[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im
+        const phoneRegex = ''
         return phoneRegex.test(number);
     }
 
@@ -32,5 +96,9 @@ export default function handleContactFormSubmission () {
             return false;
         }
         return true;
+    }
+
+    function showInfo() {
+
     }
 }
